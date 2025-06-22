b6d50ec44b3d46245191e88f0a0aa6fd05ed5fb0
JATQK <github@juliuskonrad.de>
2025-06-22 14:36:20 +0200

    Add reviewers and mergedBy filters to issue filter
    Introduces two new boolean fields, enableIssueReviewers and enableIssueMergedBy, to GithubIssueRepositoryFilter. Updates the ENABLED and DISABLED presets and the doesContainAtLeastOneEnabledFilterOption method to include these new filter options.

--- src/main/java/de/leipzig/htwk/gitrdf/database/common/entity/GithubIssueRepositoryFilter.java
+++ src/main/java/de/leipzig/htwk/gitrdf/database/common/entity/GithubIssueRepositoryFilter.java
@@ -11,74 +11,84 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class GithubIssueRepositoryFilter {
 
-    public static final GithubIssueRepositoryFilter ENABLED = new GithubIssueRepositoryFilter(
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true);
+        public static final GithubIssueRepositoryFilter ENABLED = new GithubIssueRepositoryFilter(
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true);
 
-    public static final GithubIssueRepositoryFilter DISABLED = new GithubIssueRepositoryFilter(
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false);
+        public static final GithubIssueRepositoryFilter DISABLED = new GithubIssueRepositoryFilter(
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false,
+                        false);
 
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    //private Long id;
+        // @Id
+        // @GeneratedValue(strategy = GenerationType.IDENTITY)
+        // private Long id;
 
-    private boolean enableIssueId;
+        private boolean enableIssueId;
 
-    private boolean enableIssueNumber;
+        private boolean enableIssueNumber;
 
-    private boolean enableIssueState;
+        private boolean enableIssueState;
 
-    private boolean enableIssueTitle;
+        private boolean enableIssueTitle;
 
-    private boolean enableIssueBody;
+        private boolean enableIssueBody;
 
-    private boolean enableIssueUser;
+        private boolean enableIssueUser;
 
-    private boolean enableIssueLabels;
+        private boolean enableIssueLabels;
 
-    private boolean enableIssueAssignees;
+        private boolean enableIssueAssignees;
 
-    private boolean enableIssueMilestone;
+        private boolean enableIssueMilestone;
 
-    private boolean enableIssueCreatedAt;
+        private boolean enableIssueCreatedAt;
 
-    private boolean enableIssueUpdatedAt;
+        private boolean enableIssueUpdatedAt;
 
-    private boolean enableIssueClosedAt;
+        private boolean enableIssueClosedAt;
 
-    public boolean doesContainAtLeastOneEnabledFilterOption() {
-        return this.isEnableIssueId()
-                || this.isEnableIssueNumber()
-                || this.isEnableIssueState()
-                || this.isEnableIssueTitle()
-                || this.isEnableIssueBody()
-                || this.isEnableIssueUser()
-                || this.isEnableIssueLabels()
-                || this.isEnableIssueAssignees()
-                || this.isEnableIssueMilestone()
-                || this.isEnableIssueCreatedAt()
-                || this.isEnableIssueUpdatedAt()
-                || this.isEnableIssueClosedAt();
-    }
+        private boolean enableIssueReviewers;
+
+        private boolean enableIssueMergedBy;
+
+        public boolean doesContainAtLeastOneEnabledFilterOption() {
+                return this.isEnableIssueId()
+                                || this.isEnableIssueNumber()
+                                || this.isEnableIssueState()
+                                || this.isEnableIssueTitle()
+                                || this.isEnableIssueBody()
+                                || this.isEnableIssueUser()
+                                || this.isEnableIssueLabels()
+                                || this.isEnableIssueAssignees()
+                                || this.isEnableIssueMilestone()
+                                || this.isEnableIssueCreatedAt()
+                                || this.isEnableIssueUpdatedAt()
+                                || this.isEnableIssueClosedAt()
+                                || this.isEnableIssueReviewers()
+                                || this.isEnableIssueMergedBy();
+        }
 }
